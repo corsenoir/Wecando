d387be2d51c009830be0a063d006117dec21e8f3
corsenoir <bonaparte1@gmail.com>
2026-03-29 11:37:48 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,19 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+export default defineConfig({
+  plugins: [
+    {
+      name: 'figma-asset-stub',
+      resolveId(id) {
+        if (id.startsWith('figma:asset/')) return id
+      },
+      load(id) {
+        if (id.startsWith('figma:asset/')) {
+          return `export default "/placeholder.png"`
+        }
+      }
+    }
+  ]
+})
